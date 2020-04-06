d433a0d480f133f9b072c11e83f3add464065fb8
Diane Crame <dcrame@icloud.com>
2020-04-06 14:57:16 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,25 @@
 def starts_with_a_vowel?(word)
-
+   if word.match(/^[aeiouAEIOU]/) 
+    return true
+   else
+    return false
+   end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.split(" ").map do|word|
+    word.scan(/^un.*ing$/i)
+  end.flatten
 end
 
 def words_five_letters_long(text)
-
+  text.split(" ").collect do |word|
+    word.scan(/^[a-zA-Z]{5}$/)
+  end.flatten
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    text.match?(/\A[A-Z].*.$/)
 end
 
 def valid_phone_number?(phone)
